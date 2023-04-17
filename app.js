f9eae75ea2b4db33e7abb957fcbc70dc8bdb6dc6
UnderTheKnife <cfgcl16@gmail.com>
2023-04-17 18:05:18 +0300

    Added static index.html

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var path = require('path');
-var engine = require('ejs-locals');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-var app = express();
+const convertToStatic = require("ejs-static-converter")
 
 app.set('view engine', 'ejs');
 
@@ -11,28 +11,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use('/bootstrap-icons', express.static(__dirname + '/node_modules/bootstrap-icons/font'));
 
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
-
-
-
 app.get('/', function(request,response){
-	response.redirect('/photos');
-});
-
-app.get('/photos', function(request,response){
-	response.render('index');
-});
-
-app.get('/videos', function(request,response){
-	response.render('index');
-});
-
-app.get('/podcasts', function(request,response){
 	response.render('index');
 });
 
 app.listen(3000,function(){
 	console.log('Listening at port 3000...');
 });
+
+const pages = ["index"];
+
+convertToStatic(pages);
 
 module.exports = app;
